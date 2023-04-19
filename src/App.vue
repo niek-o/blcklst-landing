a8426a8d5a71673269608c92dc4e8567afa6eb94
Niek <frankenstein795@gmail.com>
2023-04-19 22:53:49 +0200

    Improve SEO, add favicon and cover art

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
     <div class="parallax overflow-hidden md:overflow-y-visible">
-        <img src="/img/bg.jpg" class="parallax-layer layer1 h-screen sm:h-full w-screen md:w-full sm:h-auto -z-10">
-        <img src="/img/bg.jpg" class="parallax-layer layer1 mt-[140vh] lg:mt-[90vh] xl:mt-[30vh] md:w-full h-auto -z-10">
+        <img alt="background" src="/img/bg.jpg" class="parallax-layer layer1 h-screen sm:h-full w-screen md:w-full sm:h-auto -z-10">
+        <img alt="background" src="/img/bg.jpg" class="parallax-layer layer1 mt-[140vh] lg:mt-[90vh] xl:mt-[30vh] md:w-full h-auto -z-10">
 
         <div class="hero z-10 parallax-layer layer1 mt-32 md:mt-64">
-            <h1 class=" text-white text-xl font-bold text-center">EVATIA</h1>
-            <h1 class=" text-white text-6xl md:text-9xl font-bold text-center">BLCKLST</h1>
+            <h1 class="text-white text-xl font-bold text-center">EVATIA</h1>
+            <h1 class="text-white text-6xl md:text-9xl font-bold text-center">BLCKLST</h1>
         </div>
 
         <div class="hero z-10 parallax-layer layer1 mt-[30rem] md:mt-[150rem] flex w-full justify-center place-items-center">
@@ -17,8 +13,8 @@
                class="p-3 text-white text-3xl font-bold text-center border-4 border-white">VIEW TRANSMISSION LOG</a>
         </div>
 
-        <img src="/img/mako.png" class="parallax-layer layer2 w-64 h-auto -z-10">
-        <img src="/img/planet.png" class="parallax-layer layer3 w-[100rem] h-auto right-96">
+        <img alt="mako" src="/img/mako.png" class="parallax-layer layer2 w-64 h-auto -z-10">
+        <img alt="planet" src="/img/planet.png" class="parallax-layer layer3 w-[100rem] h-auto right-96">
     </div>
 
 
