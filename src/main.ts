7722e2bdf4dc41f7a000906709089bbda556e1f8
Niek <frankenstein795@gmail.com>
2023-04-19 22:43:31 +0200

    Optimise packages, remove firebase

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
 
 import App from "./App.vue";
 
@@ -10,19 +6,4 @@ import "./assets/main.css";
 
 const app = createApp(App);
 
-app.use(createPinia());
-
 app.mount("#app");
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDpoKXDRXPjS2EzhenExlVHiSdRDK3qdV8",
-    authDomain: "onstage-quiz.firebaseapp.com",
-    projectId: "onstage-quiz",
-    storageBucket: "onstage-quiz.appspot.com",
-    messagingSenderId: "115919689976",
-    appId: "1:115919689976:web:d257a7d4feaceb5b8dc30d",
-    measurementId: "G-N9WVPH2GW3"
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-getAnalytics(firebaseApp);
